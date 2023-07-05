9390a32f6330f6d8bb89e9b484e70953ba28037d
Ruslan Migirov <trapi78@gmail.com>
2023-07-05 20:20:27 +0300

    Fix typo

--- Formula/librx888.rb
+++ Formula/librx888.rb
@@ -2,7 +2,7 @@
 class Librx888 < Formula
     desc "Library for RX888 SDR"
     homepage "https://github.com/cozycactus/librx888"
-    head "https://github.com/cozycactus/librx888.git", , branch: "main"
+    head "https://github.com/cozycactus/librx888.git", branch: "main"
   
     depends_on "cmake" => :build
     depends_on "libusb"
