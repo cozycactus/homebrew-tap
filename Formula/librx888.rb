df3bce051ff884385d661bc79e0cd377e609484f
Ruslan Migirov <trapi78@gmail.com>
2023-07-05 20:14:08 +0300

    Add librx888 formula

---
+++ Formula/librx888.rb
@@ -0,0 +1,21 @@
+# librx888.rb
+class Librx888 < Formula
+    desc "Library for RX888 SDR"
+    homepage "https://github.com/cozycactus/librx888"
+    head "https://github.com/cozycactus/librx888.git"
+  
+    depends_on "cmake" => :build
+    depends_on "libusb"
+  
+    def install
+      system "mkdir", "build"
+      system "cd", "build"
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+    end
+  
+    test do
+      system "false"
+    end
+  end
