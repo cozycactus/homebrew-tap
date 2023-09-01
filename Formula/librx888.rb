6ba7521e33d560c4ba2a3d7aa1d50bed3df0e928
Ruslan Migirov <trapi78@gmail.com>
2023-09-01 21:58:14 +0300

    Add first release

--- Formula/librx888.rb
+++ Formula/librx888.rb
@@ -2,9 +2,12 @@
 class Librx888 < Formula
     desc "Library for RX888 SDR"
     homepage "https://github.com/cozycactus/librx888"
+    url "https://github.com/cozycactus/librx888/archive/refs/tags/v1.0.0.tar.gz"
+    license "GPL-2.0"
     head "https://github.com/cozycactus/librx888.git", branch: "main"
   
     depends_on "cmake" => :build
+    depends_on "pkg-config" => :build
     depends_on "libusb"
   
     def install
