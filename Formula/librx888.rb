7cbf5abd5136026c3b1ed21816fe85f41e427ad4
Ruslan Migirov <trapi78@gmail.com>
2023-09-01 22:00:16 +0300

    Add sha256 sum

--- Formula/librx888.rb
+++ Formula/librx888.rb
@@ -3,6 +3,7 @@ class Librx888 < Formula
     desc "Library for RX888 SDR"
     homepage "https://github.com/cozycactus/librx888"
     url "https://github.com/cozycactus/librx888/archive/refs/tags/v1.0.0.tar.gz"
+    sha256 "a00046443470384cd65b09f681a0636f286276048bf939d8dd94bda049dd4467"
     license "GPL-2.0"
     head "https://github.com/cozycactus/librx888.git", branch: "main"
   
