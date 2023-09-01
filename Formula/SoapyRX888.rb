15f118bfda39b1bca376e540c5afcbdbeaa4d6d6
Ruslan Migirov <trapi78@gmail.com>
2023-09-01 22:34:31 +0300

    Add sha

--- Formula/SoapyRX888.rb
+++ Formula/SoapyRX888.rb
@@ -2,6 +2,7 @@ class Soapyrx888 < Formula
   desc "Soapy SDR plugin for the RX-888"
   homepage "https://github.com/cozycactus/SoapyRX888"
   url "https://github.com/cozycactus/SoapyRX888/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "a0f8cc08b7c87757e19e7270382366841973d32a37999c126d205d30c2381b64"
   head "https://github.com/cozycactus/SoapyRX888.git", branch: "main"
 
     depends_on "cmake" => :build
