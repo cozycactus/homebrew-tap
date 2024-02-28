05dd808cd7f4837f68dcf5af49fdf86ca4d0b10a
Ruslan Migirov <trapi78@gmail.com>
2024-02-28 13:29:59 +0200

    Add ngspice formula
    Edited official formula to fix kicad build

---
+++ Formula/ngspice.rb
@@ -0,0 +1,62 @@
+class Ngspice < Formula
+  desc "Spice circuit simulator"
+  homepage "https://ngspice.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/ngspice/ng-spice-rework/42/ngspice-42.tar.gz"
+  sha256 "737fe3846ab2333a250dfadf1ed6ebe1860af1d8a5ff5e7803c772cc4256e50a"
+  license :cannot_represent
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/ngspice[._-]v?(\d+(?:\.\d+)*)\.t}i)
+  end
+
+  head do
+    url "https://git.code.sf.net/p/ngspice/ngspice.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "fftw"
+  depends_on "libngspice"
+  depends_on "readline"
+
+  uses_from_macos "bison" => :build
+
+  def install
+    system "./autogen.sh" if build.head?
+
+    args = %w[
+      --with-readline=yes
+      --enable-xspice
+      --without-x
+      --with-ngshared
+    ]
+
+    system "./configure", *args, *std_configure_args
+    system "make", "install"
+
+    # fix references to libs
+    inreplace pkgshare/"scripts/spinit", lib/"ngspice/", Formula["libngspice"].opt_lib/"ngspice/"
+
+    # remove conflict lib files with libngspice
+    rm_rf Dir[lib/"ngspice"]
+  end
+
+  test do
+    (testpath/"test.cir").write <<~EOS
+      RC test circuit
+      v1 1 0 1
+      r1 1 2 1
+      c1 2 0 1 ic=0
+      .tran 100u 100m uic
+      .control
+      run
+      quit
+      .endc
+      .end
+    EOS
+    system "#{bin}/ngspice", "test.cir"
+  end
+end
